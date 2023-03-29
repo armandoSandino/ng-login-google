a05fd50d1f1847073718b620d94315c5518bb1ac
ArmandoSandino <jsandino@arkkosoft.net>
2023-03-29 09:40:43 -0600

    refactor feat; change login interface, for button login google auth

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,4 +1,4 @@
-import { NgModule } from '@angular/core';
+import { CUSTOM_ELEMENTS_SCHEMA, NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
@@ -33,6 +33,9 @@ import { CommonModule } from '@angular/common';
     AppRoutingModule
   ],
   exports: [],
+  schemas: [
+    CUSTOM_ELEMENTS_SCHEMA
+  ],
   providers: [
     {
       provide: 'SocialAuthServiceConfig',
